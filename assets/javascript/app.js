0e36f2621fd69d4bdc791a3fadb9039a43be5d55
Ann Simeoli <asimeoli@gmail.com>
2021-06-12 19:38:25 -0400

    edit to register

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+//global variables
+
+function registrationComplete() {
+	document.getElementById("registrationTitle").innerHTML = "Registration Complete";
+	document.getElementById("registrationText").innerHTML = "Thank you!";
+	document.getElementById("registrationFormDiv").innerHTML = "You are now registered for the conference. To cancel your registration please press -"
+
+}
+
+
+	
