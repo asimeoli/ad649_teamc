5d6344eabcca40e6517c9ccc98481c53ed4c4205
Ann Simeoli <asimeoli@gmail.com>
2021-06-12 22:34:31 -0400

    form cancel button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,20 @@
-//global variables
-
+//submit form
 function registrationComplete() {
-	document.getElementById("registrationTitle").innerHTML = "Registration Complete";
-	document.getElementById("registrationText").innerHTML = "Thank you!";
-	document.getElementById("registrationFormDiv").innerHTML = "You are now registered for the conference. To cancel your registration please press -"
+  document.getElementById("registrationTitle").innerHTML =
+    "Registration Complete";
 
+  document.getElementById("registrationText").innerHTML = "Thank you!";
+
+  document.getElementById("registrationForm").innerHTML =
+    "<div class='card text-center border border-white' id='afterForm'><h3>You are now registered for the conference. </h3 <br> <p>To cancel your registration please click here:</p><br><button type='button' onClick='cancelComplete()' class='btn btn-danger btn-block'>Cancel</button>";
 }
 
+//cancel form
+function cancelComplete() {
+  location.reload();
+}
 
-	
+//form info
+function printInfo() {
+  console.log(inputName);
+}
