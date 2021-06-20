7f5520c134ec5a395de30e121adc957e20caec54
Ann Simeoli <asimeoli@gmail.com>
2021-06-20 15:01:52 -0400

    link to agile lab button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,7 +6,7 @@ function registrationComplete() {
   document.getElementById("registrationText").innerHTML = "Thank you!";
 
   document.getElementById("registrationForm").innerHTML =
-    "<div class='card text-center border border-white' id='afterForm'><h3>You are now registered for the conference. </h3 <br> <p>To cancel your registration please click here:</p><br><button type='button' onClick='cancelComplete()' class='btn btn-danger btn-block'>Cancel</button>";
+    "<div class='card text-center border border-white' id='afterForm'><h3>You are now registered for the conference.</h3<br><p>To cancel your registration please click here:</p><button type='button' onClick='cancelComplete()' class='btn btn-danger btn-block'>Cancel</button>";
 }
 
 //cancel form
